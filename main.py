dbf7a941c0b7b193824283aebb23754f93825519
nidai <daichi.ni1460@gmail.com>
2022-05-29 21:41:16 +0900

    3rd commit.

---
+++ main.py
@@ -0,0 +1,25 @@
+from swctrl import gpioSwitch
+import time
+import sys
+
+# ボタン
+INPUT_PIN_PORT = 26
+
+def main():
+
+    changeSw = gpioSwitch.GpioSwitch(INPUT_PIN_PORT)
+
+    while True:
+
+        changeSw.update()
+
+        print("PinStatus : " + changeSw.getPinStatus())
+
+        #少し待つ
+        time.sleep(1)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+       print(sys.exc_info())
---
+++ swctrl/gpioSwitch.py
@@ -0,0 +1,22 @@
+
+#GPIOの初期設定
+import RPi.GPIO as GPIO
+
+class GpioSwitch():
+
+    m_inputPin = 0
+
+    def __init__(self, inputPin):
+        # 初期化
+        m_inputPin = inputPin
+        GPIO.setmode(GPIO.BCM)
+        #入力端子設定
+        GPIO.setup(self.m_inputPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    def update(self):
+        #スイッチ状態更新
+        sw_status = GPIO.input(self.m_inputPin)
+
+    def getPinStatus(self):
+        #スイッチ状態取得
+        return self.m_inputPin
