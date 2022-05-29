05032bef80cfcba8ef0251b196a06155d2a7602e
nidai <daichi.ni1460@gmail.com>
2022-05-29 22:23:46 +0900

    6th commit.

--- swctrl/gpioSwitch.py
+++ swctrl/gpioSwitch.py
@@ -4,30 +4,35 @@ import RPi.GPIO as GPIO
 
 SW_ON = 0
 SW_OFF = 1
-GPIO.setmode(GPIO.BCM)
 
 class GpioSwitch():
 
-    m_inputPin = 0
+    m_chageSwPin = 0
+    m_exitSwPin = 0
+    m_chageSwStatus = 0
+    m_exitSwStatus = 0
 
     def __init__(self, chageSwPin, exitSwPin):
+        self.m_chageSwPin = chageSwPin
+        self.m_exitSwPin = exitSwPin
+
         # 初期化
-        m_chageSwPin = chageSwPin
-        m_exitSwPin = exitSwPin
+        GPIO.setmode(GPIO.BCM)
         #入力端子設定
-        GPIO.setup([self.m_inputPin, self.m_exitSwPin], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup([self.m_chageSwPin, self.m_exitSwPin], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def __del__(self):
         GPIO.cleanup()
 
     def update(self):
         #スイッチ状態更新
-        self.m_inputPin = GPIO.input(self.m_inputPin)
+        self.m_chageSwStatus = GPIO.input(self.m_chageSwPin)
+        self.m_exitSwStatus = GPIO.input(self.m_exitSwPin)
 
     def getChangeSwStatus(self):
         #スイッチ状態取得
-        return self.m_inputPin
+        return self.m_chageSwStatus
 
-    def getChangeSwStatus(self):
+    def getExitSwStatus(self):
         #スイッチ状態取得
-        return self.m_inputPin
+        return self.m_exitSwStatus
