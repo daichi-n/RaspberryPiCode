28a97454c88d8d69f1b93f7f40f9e7d0eb9cc121
daichi-n <daichi.ni1460@gmail.com>
2022-05-29 22:21:03 +0900

    5th commit.

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import time
 import sys
 
 # ボタン
-INPUT_PIN_PORT = 26
+CHANGE_BUTTON_PIN = 26
+EXIT_BUTTON_PIN = 4
 
 def main():
 
-    changeSw = gpioSwitch.GpioSwitch(INPUT_PIN_PORT)
+    sw = gpioSwitch.GpioSwitch(CHANGE_BUTTON_PIN, EXIT_BUTTON_PIN)
 
     while True:
 
-        changeSw.update()
+        sw.update()
 
-        print("PinStatus : " + str(changeSw.getPinStatus()))
+        print("PinStatus : " + str(sw.getChangeSwStatus()))
+
+        if sw.getExitSwStatus() == gpioSwitch.SW_ON:
+            print("Exit Application.")
+            return
 
         #少し待つ
         time.sleep(1)
@@ -22,4 +27,5 @@ if __name__ == '__main__':
     try:
         main()
     except:
-       print(sys.exc_info())
+        print("Exception Occured!!")
+        print(sys.exc_info())
--- swctrl/gpioSwitch.py
+++ swctrl/gpioSwitch.py
@@ -2,21 +2,32 @@
 #GPIOの初期設定
 import RPi.GPIO as GPIO
 
+SW_ON = 0
+SW_OFF = 1
+GPIO.setmode(GPIO.BCM)
+
 class GpioSwitch():
 
     m_inputPin = 0
 
-    def __init__(self, inputPin):
+    def __init__(self, chageSwPin, exitSwPin):
         # 初期化
-        m_inputPin = inputPin
-        GPIO.setmode(GPIO.BCM)
+        m_chageSwPin = chageSwPin
+        m_exitSwPin = exitSwPin
         #入力端子設定
-        GPIO.setup(self.m_inputPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup([self.m_inputPin, self.m_exitSwPin], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    def __del__(self):
+        GPIO.cleanup()
 
     def update(self):
         #スイッチ状態更新
         self.m_inputPin = GPIO.input(self.m_inputPin)
 
-    def getPinStatus(self):
+    def getChangeSwStatus(self):
+        #スイッチ状態取得
+        return self.m_inputPin
+
+    def getChangeSwStatus(self):
         #スイッチ状態取得
         return self.m_inputPin
