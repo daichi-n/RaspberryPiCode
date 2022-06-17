521ea73baf030d5adf548c39b87dcf544ef8838d
nidai <daichi.ni1460@gmail.com>
2022-06-18 01:38:12 +0900

    minor changes.

--- lcd/lcd.py
+++ lcd/lcd.py
@@ -1,10 +1,3 @@
-# _____ _____ _____ __ __ _____ _____ 
-#|     |   __|     |  |  |     |     |
-#|  |  |__   |  |  |_   _|  |  |  |  |
-#|_____|_____|_____| |_| |_____|_____|
-#
-# Project Tutorial Url:http://osoyoo.com/?p=1031
-#  
 import smbus
 import time
 
@@ -30,80 +23,77 @@ ENABLE = 0b00000100 # Enable bit
 E_PULSE = 0.0005
 E_DELAY = 0.0005
 
-#Open I2C interface
-#bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
-bus = smbus.SMBus(1) # Rev 2 Pi uses 1
+class LcDisplay():
+    #Open I2C interface
+    #bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
+    m_bus = smbus.SMBus(1) # Rev 2 Pi uses 1
 
-def lcd_init():
-  # Initialise display
-  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
-  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
-  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
-  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off 
-  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
-  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
-  time.sleep(E_DELAY)
+    def __init__(self):
+        # Initialise display
+        self.lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+        self.lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+        self.lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+        self.lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off 
+        self.lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+        self.lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+        time.sleep(E_DELAY)
 
-def lcd_byte(bits, mode):
-  # Send byte to data pins
-  # bits = the data
-  # mode = 1 for data
-  #        0 for command
+    def __del__(self):
+        self.lcd_byte(0x01, LCD_CMD)
 
-  bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
-  bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
+    def lcd_byte(self, bits, mode):
+        # Send byte to data pins
+        # bits = the data
+        # mode = 1 for data
+        #        0 for command
 
-  # High bits
-  bus.write_byte(I2C_ADDR, bits_high)
-  lcd_toggle_enable(bits_high)
+        bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
+        bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
 
-  # Low bits
-  bus.write_byte(I2C_ADDR, bits_low)
-  lcd_toggle_enable(bits_low)
+        # High bits
+        self.m_bus.write_byte(I2C_ADDR, bits_high)
+        self.lcd_toggle_enable(bits_high)
 
-def lcd_toggle_enable(bits):
-  # Toggle enable
-  time.sleep(E_DELAY)
-  bus.write_byte(I2C_ADDR, (bits | ENABLE))
-  time.sleep(E_PULSE)
-  bus.write_byte(I2C_ADDR,(bits & ~ENABLE))
-  time.sleep(E_DELAY)
+        # Low bits
+        self.m_bus.write_byte(I2C_ADDR, bits_low)
+        self.lcd_toggle_enable(bits_low)
 
-def lcd_string(message,line):
-  # Send string to display
+    def lcd_toggle_enable(self, bits):
+        # Toggle enable
+        time.sleep(E_DELAY)
+        self.m_bus.write_byte(I2C_ADDR, (bits | ENABLE))
+        time.sleep(E_PULSE)
+        self.m_bus.write_byte(I2C_ADDR,(bits & ~ENABLE))
+        time.sleep(E_DELAY)
 
-  message = message.ljust(LCD_WIDTH," ")
+    def lcd_string(self, message, line):
+        # Send string to display
 
-  lcd_byte(line, LCD_CMD)
+        message = message.ljust(LCD_WIDTH," ")
 
-  for i in range(LCD_WIDTH):
-    lcd_byte(ord(message[i]),LCD_CHR)
+        self.lcd_byte(line, LCD_CMD)
+
+        for i in range(LCD_WIDTH):
+            self.lcd_byte(ord(message[i]),LCD_CHR)
 
 def main():
-  # Main program block
+    lcd = LcDisplay()
 
-  # Initialise display
-  lcd_init()
+    while True:
+        # Send some test
+        lcd.lcd_string("Created by  <",LCD_LINE_1)
+        lcd.lcd_string("Osoyoo.com  <",LCD_LINE_2)
 
-  while True:
+        time.sleep(3)
 
-    # Send some test
-    lcd_string("Created by  <",LCD_LINE_1)
-    lcd_string("Osoyoo.com  <",LCD_LINE_2)
+        # Send some more text
+        lcd.lcd_string("> Tutorial Url:",LCD_LINE_1)
+        lcd.lcd_string("> http://osoyoo.com",LCD_LINE_2)
 
-    time.sleep(3)
-  
-    # Send some more text
-    lcd_string("> Tutorial Url:",LCD_LINE_1)
-    lcd_string("> http://osoyoo.com",LCD_LINE_2)
-
-    time.sleep(3)
+        time.sleep(3)
 
 if __name__ == '__main__':
-
-  try:
-    main()
-  except KeyboardInterrupt:
-    pass
-  finally:
-    lcd_byte(0x01, LCD_CMD)
+    try:    
+        main()
+    except KeyboardInterrupt:
+        pass
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from swctrl import gpioSwitch
 from stepperMoter import stepperMoter
 from length_monitor import length
+from lcd import lcd
 import time
 import sys
 
@@ -9,6 +10,7 @@ def main():
     sw = gpioSwitch.GpioSwitch()
     StepMoter = stepperMoter.C28BYJ48(IN1=5, IN2=6, IN3=13, IN4=19)
     length_mon = length.LengthMonitor()
+    lc_display = lcd.LcDisplay()
 
     flag = False
 
@@ -33,10 +35,12 @@ def main():
                 flag = True
 
         distance = length_mon.mesure()
-        print ( "Discance : {:.1f} cm".format( distance ) )
+
+        lc_display.lcd_string("Discance ", lcd.LCD_LINE_1)
+        lc_display.lcd_string("{:.1f} cm".format( distance ), lcd.LCD_LINE_2)
 
         #少し待つ
-        time.sleep(1)
+        time.sleep(3)
 
 if __name__ == '__main__':
     try:
